7c0dfb7d5d774a656c47eaadf3327ba03a72f454
dollarkillerx <adapawang@gmail.com>
2021-07-07 18:54:55 +0800

    :sparkles: cryptography

---
+++ cryptology/aes.go
@@ -0,0 +1,56 @@
+package cryptology
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+// AESEncrypt AES加密
+func AESEncrypt(key []byte, plaintext []byte) ([]byte, error) {
+	if len(key) != 16 && len(key) != 32 {
+		return nil, errors.New("key != 16 or != 32")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, err
+}
+
+// AESDecrypt AES 解密
+func AESDecrypt(key []byte, ciphertext []byte) ([]byte, error) {
+	if len(key) != 16 && len(key) != 32 {
+		return nil, errors.New("key != 16 or != 32")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, errors.New("ciphertext < aes.BlockSize")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return ciphertext, err
+}
---
+++ cryptology/cryptogy_test.go
@@ -0,0 +1,50 @@
+package cryptology
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestAES(t *testing.T) {
+	key := []byte("12345678912345s612345678912345s6")
+	val := []byte("hello world dsads1456484848948adsadsadsadsads1456484848948adsadsadsadsads1456484848948adsadsadsa")
+	fmt.Println(len(val))
+	encrypt, err := AESEncrypt(key, val)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	fmt.Println(len(encrypt))
+
+	decrypt, err := AESDecrypt(key, encrypt)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	fmt.Println(string(decrypt))
+}
+
+func TestRSA(t *testing.T) {
+	privateKey, pubKey, _ := GenRsaKey()
+	fmt.Println("key: ", string(privateKey))
+	fmt.Println("pubKey: ", string(pubKey))
+
+	// 签名
+	data1 := "tESt rsa 1212"
+	sha256, _ := RsaSignWithSha256([]byte(data1), privateKey)
+	fmt.Println("sh: ", string(sha256))
+
+	// 验签
+	fmt.Println(RsaVerySignWithSha256([]byte(data1), sha256, pubKey))
+
+	// 加密
+
+	encrypt, _ := RsaEncrypt([]byte(data1), pubKey)
+	fmt.Println("en: ", encrypt, "  ", len(encrypt), "  org: ", len([]byte(data1)))
+
+	// 解密
+	decrypt, _ := RsaDecrypt(encrypt, privateKey)
+	fmt.Println(string(decrypt))
+}
